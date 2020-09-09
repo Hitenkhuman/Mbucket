809db154779f8d30fab667743eeab83fceb1f5ac
Hiten khuman <hitenkhuman15@gmail.com>
2020-09-09 18:47:23 +0530

    footer

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
   <title>Home Page</title>
   <link rel="stylesheet" href="customcss.css">
 </head>
-
 <body>
   <nav class="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -157,121 +156,15 @@
   </div>
 
   <br>
-  
-<footer class="page-footer font-small bg-light text-black">
+  <?php
+  include "footer.php";
+  ?>
 
-<!-- Footer Links -->
-<div class="container text-center text-md-left">
-
-  <!-- Footer links -->
-  <div class="row text-center text-md-left mt-3 pb-3">
-
-    <!-- Grid column -->
-    <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
-      <h6 class="text-uppercase mb-4 font-weight-bold">About us</h6><hr>
-      <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </div>
-    <!-- Grid column -->
-
-    <hr class="w-100 clearfix d-md-none">
-
-    <!-- Grid column -->
-    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-      <h6 class="text-uppercase mb-4 font-weight-bold">quick search</h6><hr>
-      <p>
-        <a href="#">Your Account</a>
-      </p>
-      <p>
-        <a href="#">Become an Affiliate</a>
-      </p>
-      <p>
-        <a href="#">Shipping Rates</a>
-      </p>
-      <p>
-        <a href="#">Help</a>
-      </p>
-    </div>
-
-    <!-- Grid column -->
-    <hr class="w-100 clearfix d-md-none">
-
-    <!-- Grid column -->
-    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-      <h6 class="text-uppercase mb-4 font-weight-bold">Contact us</h6><hr>
-      <p>
-        <i class="fas fa-home mr-3"></i> Gujarat, India</p>
-      <p>
-        <i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
-      <p>
-        <i class="fas fa-phone mr-3"></i> +91 97238 21144<br>
-        <i class="fas fa-print mr-3"></i> +91 72848 82289</p>
-    </div>
-    <!-- Grid column -->
-
-  </div>
-  <!-- Footer links -->
-
-  <hr>
-
-  <!-- Grid row -->
-  <div class="row d-flex align-items-center">
-
-    <!-- Grid column -->
-    <div class="col-md-7 col-lg-8">
-
-      <!--Copyright-->
-      <p class="text-center text-md-left">© 2020 Copyright:
-        <a href="https://mdbootstrap.com/">
-          <strong> MDBootstrap.com</strong>
-        </a>
-      </p>
-
-    </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-md-5 col-lg-4 ml-lg-0">
-
-      <!-- Social buttons -->
-      <div class="text-center text-md-right">
-        <ul class="list-unstyled list-inline">
-          <li class="list-inline-item">
-            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-          </li>
-          <li class="list-inline-item">
-            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-              <i class="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li class="list-inline-item">
-            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-              <i class="fab fa-google-plus-g"></i>
-            </a>
-          </li>
-          <li class="list-inline-item">
-            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-              <i class="fab fa-linkedin-in"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-
-    </div>
-    <!-- Grid column -->
-
-  </div>
-  <!-- Grid row -->
-
-</div>
-<!-- Footer Links -->
-
-</footer>
 
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
+
 
 </html>
