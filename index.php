0ff564452be85cd3f36b6e823390ecd1b05685d2
Hiten khuman <hitenkhuman15@gmail.com>
2020-09-15 14:51:09 +0530

    colorschange

--- index.php
+++ index.php
@@ -7,19 +7,19 @@
   <script src="https://kit.fontawesome.com/4dedb1023f.js" crossorigin="anonymous"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home Page</title>
-  <link rel="stylesheet" href="../css/customcss.css">
+  <link rel="stylesheet" href="./css/home.css">
 </head>
 
 <body>
-  <header class="bg-primary">
-  <div class="row p-2">
-    <div class="col-1 d-sm-none">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <!-- <span class="navbar-toggler-icon"></span> -->
-      <i class="fas fa-bars"></i>
-    </button>
-    </div>
-    
+  <header class="headernav">
+    <div class="row p-2">
+      <div class="col-1 d-sm-none">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <!-- <span class="navbar-toggler-icon"></span> -->
+          <i class="fas fa-bars"></i>
+        </button>
+      </div>
+
       <div class="col-6  col-md-2">
         <a href=""></a>
         here is logo
@@ -34,11 +34,11 @@
       </div>
 
       <div class="col-2 col-md-1 order-1 order-md-11">
-        
-          <a href="signin.php" class="text-secondary">
-            <span class="fas fa-user-plus"> &nbsp;</span>
-          </a>
-        
+
+        <a href="signin.php" style="color: white;">
+          <span class="fas fa-user-plus"> &nbsp;</span>
+        </a>
+
       </div>
       <div class="col-2 col-md-1 order-2 order-md-12">
         <span class="navbar-text">
@@ -55,15 +55,12 @@
 
   </header>
 
-  <nav class="navbar navbar-expand-sm navbar-dark bg-secondary">
-    
-
-
+  <nav class="navbar navbar-expand-sm">
 
     <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item active pl-5">
-          <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+        <li class="nav-item pl-5">
+          <a class="nav-link current " href="#">Home<span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item pl-5">
           <a class="nav-link" href="#">Mobiles</a>
@@ -166,44 +163,177 @@
   </div>
   <div class="info">
     <div class="container-fluid">
-      <div class="row">
-        <div class="col-12 col-md-6 col-lg-3 p-sm-5">
-          <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4">
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4">
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-6 col-lg-3 p-sm-5">
-          <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-6 col-lg-3 p-sm-5">
-          <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-6 col-lg-3 p-sm-5">
-          <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-9 col-md-3 col-lg-4 p-sm-5">
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
             </div>
           </div>
         </div>
@@ -212,10 +342,121 @@
   </div>
 
   <br>
-  <?php
-  include "footer.php";
-  ?>
+ 
+  <footer class="page-footer font-small text-black">
 
+<!-- Footer Links -->
+<div class="container text-center text-md-left">
+
+  <!-- Footer links -->
+  <div class="row text-center text-md-left mt-3 pb-3">
+
+    <!-- Grid column -->
+    <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
+      <h6 class="text-uppercase mb-4 font-weight-bold">About us</h6><hr>
+      <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+    <!-- Grid column -->
+
+    <hr class="w-100 clearfix d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+      <h6 class="text-uppercase mb-4 font-weight-bold">Quick search</h6><hr>
+      <p>
+        <a href="#" class="footer-link">Your Account</a>
+      </p>
+      <p>
+        <a href="#"  class="footer-link">Become an Affiliate</a>
+      </p>
+      <p>
+        <a href="#"  class="footer-link">Shipping Rates</a>
+      </p>
+      <p>
+        <a href="#"  class="footer-link">Help</a>
+      </p>
+    </div>
+
+    <!-- Grid column -->
+    <hr class="w-100 clearfix d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+      <h6 class="text-uppercase mb-4 font-weight-bold">Contact us</h6><hr>
+      <p>
+        <i class="fas fa-home mr-3"></i> Gujarat, India</p>
+      <p>
+        <i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+      <p>
+        <i class="fas fa-phone mr-3"></i> +91 97238 21144<br>
+        <i class="fas fa-print mr-3"></i> +91 72848 82289</p>
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Footer links -->
+  <!-- Grid column -->
+  <div class="row justify-content-center">
+
+<!-- Social buttons -->
+<div class="text-center text-md-right">
+  <ul class="list-unstyled list-inline">
+    <li class="list-inline-item">
+      <a class="btn-floating btn-sm rgba-white-slight mx-1">
+        <i class="fab fa-facebook-f social-icon"></i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a class="btn-floating btn-sm rgba-white-slight mx-1">
+        <i class="fab fa-twitter social-icon"></i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a class="btn-floating btn-sm rgba-white-slight mx-1">
+        <i class="fab fa-google-plus-g social-icon"></i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a class="btn-floating btn-sm rgba-white-slight mx-1">
+        <i class="fab fa-linkedin-in social-icon"></i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a class="btn-floating btn-sm rgba-white-slight mx-1">
+        <i class="fab fa-instagram social-icon"></i>
+      </a>
+    </li>
+  </ul>
+</div>
+
+</div>
+<!-- Grid column -->
+
+  <hr>
+
+  <!-- Grid row -->
+  <div class="row justify-content-center">
+
+    <!-- Grid column -->
+    
+
+      <!--Copyright-->
+      <p class="text-center text-md-left">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/">
+          <strong> Mobileinfo.com</strong>
+        </a>
+      </p>
+    <!-- Grid column -->
+
+    
+
+  </div>
+  <!-- Grid row -->
+
+</div>
+<!-- Footer Links -->
+
+</footer>
 
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
